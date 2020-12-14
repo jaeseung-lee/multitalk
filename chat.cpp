1843d384c897875c8d9930f9724e30546de873f9
곰돌씨 <64863514+tony7078@users.noreply.github.com>
2020-12-15 04:39:00 +0900

    주석 추가
    주석에 자세한 설명 추가

--- chat.cpp
+++ chat.cpp
@@ -234,25 +234,27 @@ void out(){
 
 void chatList() {
     while (1) {
-        //기능 출력
+        //메뉴 출력
         int number;
         cout << ">> Chat Lists : " << endl;
         cout << "-1. exit" << endl;
         cout << "0. back" << endl;
         cout << "1. Make new Chat room" << endl;
 
-        //톡방 이름txt 파일을 읽어서 갯수에 맞게 출력
+        //chatList.txt 파일을 읽어서 톡방 갯수에 맞게 출력
         ifstream chatName;
         string chatUserName;
-
         char selectedChat[100];
         char line[100];
         int num = 2;
         chatName.open("chatList.txt");
         if (chatName.is_open()) {
             while (chatName.getline(line, sizeof(line))) {
+                //톡방 이름 chatList.txt에서 줄마다 받아와서 출력
                 cout << num << ". "
                      << "Chat Name: " << line << endl;
+
+                //톡방 멤버 받아오기, 각 채팅방에 적혀져 있는 유저 목록 불러오기
                 cout << "   Chat Member: ";
                 ifstream in(line);
                 while (getline(in, chatUserName)) {
@@ -267,12 +269,13 @@ void chatList() {
         }
         chatName.close();
 
+        //입력 받는곳
         cout << " " << endl;
         cout << ">> select number" << endl;
         cout << "<< ";
         cin >> number;
 
-        //연결 필요한 부분
+        //입력 받은 번호에 따라 선택지 갈림
         if (number == 0) {
             cout << "return to back" << endl;
             system("clear");
@@ -282,13 +285,17 @@ void chatList() {
         } else if (number == -1) {
             break;
             exit(-1);
+        //0과1과-1을 입력하지 않았다면 톡방으로 들어 가는걸로 간주
         } else {
+            //들어갈 톡방 이름을 입력 받음
             cout << ">> Write the name of chat room which you want to "
                     "join(include .txt)"
                  << endl;
             cout << "<< ";
             cin >> selectedChat;
+            //입력받은 톡방 열기
             ofstream ofs2(selectedChat, ios::app);
+            //이곳 부터 채팅 구현 파트
             ofs2 << "CHATTING" << endl;
             ofs2.close();
             exit(-1);
@@ -305,10 +312,11 @@ void chatMake() {
     cin >> ch;
 
     ofstream ofs(ch);
-
+    //벡터 컨테이너에 담긴 겍체에 접근하여 유저 리스트 먼저 보여줌
     cout << endl << ">> ( User List )" << endl;
     for (int i = 0; i < Users.size(); i++)
         cout << i + 1 << ". " << Users[i].getName() << endl;
+    //초대할 유저 고르는 파트, 0을 누르면 초대 종료
     cout << ">> Please write users you want to invite: " << endl;
     cout << "   (Write 0 to finish)" << endl;
     while (1) {
@@ -319,6 +327,7 @@ void chatMake() {
             ofs << us << endl;
         }
     }
+    //각 톡방 유저 목록을 위해 선 입력
     ofs << "END" << endl;
     ofs << "<Chat room member>" << endl;
     ofs << "-----------------" << endl;
